8fccb919742d2035d8c623fca69d7e755a36bfe2
Sulabh Ghimire <sulabh.ghimire@gritfeat.com>
2024-09-25 11:26:17 +0545

    Begining with options api

---
+++ vue-crash-course/src/App.vue
@@ -0,0 +1,43 @@
+<script>
+  // In options API need to exports the default so that we can have access to data and methods in template
+  export default {
+    data() {
+      return {
+        name: "John Doe",
+        status: "pending",
+        tasks: ["Task 1", "Task 2", "Task 3"],
+        link: 'https://www.google.com'
+      }
+    },
+    methods:{
+      toggleStatus (){
+        if(this.status === "active") this.status = "pending"
+        else if (this.status === "pending") this.status = "inactive"
+        else this.status = 'active'
+      }
+    }
+
+  }
+
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+  
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">
+      {{ task }}
+    </li>
+  </ul>
+
+  <!-- <a v-bind:href="link">Click to google</a> -->
+  <a :href="link">Click to google</a>
+
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+
+</template>
